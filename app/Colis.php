571f246f00349871f7313be441a8af6dc2be0dca
Fwmarc <160975420+marcFTGE@users.noreply.github.com>
2024-09-29 20:42:53 +0200

    Update Colis.php

--- app/Colis.php
+++ app/Colis.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Colis extends Model
 {
-    protected $fillable = ["user_id", "nature", "contenance", "nom", "quantite", "fragilite", "valeur_euro", "poids", "longueur", "hauteur", "largeur", "moyen_envoi", "country", "date_arrivee", "tarif_id", "receiver_id","who"];
+    protected $fillable = ["user_id", "nature", "contenance", "nom", "quantite", "fragilite", "valeur_euro", "poids", "longueur", "hauteur", "largeur", "moyen_envoi", "country", "date_arrivee", "tarif_id", "receiver_id","who","hour","hours"];
 
     public function user(){
         return $this->belongsTo(User::class);
